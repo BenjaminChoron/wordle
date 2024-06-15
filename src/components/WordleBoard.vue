60635883ccf7f1bed53a6499398770748d6c5d80
BenjCH <benjamin.choron@outlook.com>
2024-06-15 16:39:30 +0200

    ✨ Limit player guesses to 5 characters

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '@/settings'
 import englishWords from '@/englishWordsWith5Letters.json'
 
@@ -12,10 +12,22 @@ defineProps({
 
 const guessInProgress = ref('')
 const guessSubmitted = ref('')
+
+const formattedGuessInProgress = computed({
+  get: () => guessInProgress.value,
+  set: (rawValue: string) => {
+    guessInProgress.value = rawValue.slice(0, 5)
+  }
+})
 </script>
 
 <template>
-  <input type="text" v-model="guessInProgress" @keydown.enter="guessSubmitted = guessInProgress" />
+  <input
+    maxlength="5"
+    type="text"
+    v-model="formattedGuessInProgress"
+    @keydown.enter="guessSubmitted = guessInProgress"
+  />
   <p
     v-if="guessSubmitted.length > 0"
     v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
