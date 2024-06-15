d7da1d8f2a3e29b530e90c5f23c6f0e4b55c3cb8
BenjCH <benjamin.choron@outlook.com>
2024-06-15 15:24:20 +0200

    ✅ Only render end-of-the-game feedback after the game is over

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -32,5 +32,14 @@ describe('WordleBoard', () => {
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
 
-  test.todo('no end-of-game message appears if the user has not yet made a guess')
+  test('no end-of-game message appears if the user has not yet made a guess', () => {
+    const wrapper = mount(WordleBoard, {
+      props: {
+        wordOfTheDay
+      }
+    })
+
+    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+  })
 })
