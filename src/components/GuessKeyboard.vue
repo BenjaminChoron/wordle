cbeca4f04c7a007a470fe17b7fbdfd791751f340
BenjCH <benjamin.choron@outlook.com>
2024-06-18 13:24:06 +0200

    🚧 Create a virtual qwerty keyboard

---
+++ src/components/GuessKeyboard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { QWERTY_KEYS } from '@/settings'
+
+const qwertyKeysArray = QWERTY_KEYS.split('')
+</script>
+
+<template>
+  <div class="virtual-keyboard">
+    <button
+      v-for="key in qwertyKeysArray"
+      :key="key"
+      :aria-label="`Type ${key}`"
+      @click="$emit('type', key)"
+      class="key"
+    >
+      {{ key }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.virtual-keyboard {
+  position: fixed;
+  bottom: 1rem;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  gap: 0.2rem;
+  margin-top: 1rem;
+}
+
+.key {
+  width: 1.92rem;
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #000;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  background-color: #fff;
+  transition: background-color 0.2s;
+}
+.key:hover {
+  background-color: #f0f0f0;
+}
+</style>
